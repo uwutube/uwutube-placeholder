020415bfcd2a33f9642ade95770a3e62da081b5a
Alex <alexnoir03@gmail.com>
2020-06-03 16:02:49 +0100

    Add fetch intercept

--- service-worker.js
+++ service-worker.js
@@ -15,3 +15,40 @@ self.addEventListener("install", function (event) {
         })
     );
 });
+
+self.addEventListener("fetch", function (event) {
+    if (event.request.method !== "GET") {
+        return;
+    }
+    event.respondWith(
+        caches
+        .match(event.request)
+        .then(function (cached) {
+            var networked = fetch(event.request)
+                .then(fetchedFromNetwork, unableToResolve)
+                .catch(unableToResolve);
+            return cached || networked;
+
+            function fetchedFromNetwork(response) {
+                var cacheCopy = response.clone();
+                caches
+                    .open("uwutube-placeholder-1-pages")
+                    .then(function add(cache) {
+                        cache.put(event.request, cacheCopy);
+                    });
+
+                return response;
+            }
+
+            function unableToResolve() {
+                return new Response("<h1>Service Unavailable</h1>", {
+                    status: 503,
+                    statusText: "Service Unavailable",
+                    headers: new Headers({
+                        "Content-Type": "text/html"
+                    })
+                });
+            }
+        })
+    );
+});
